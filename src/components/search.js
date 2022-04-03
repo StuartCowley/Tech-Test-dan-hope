94432b40d8c808b9efa560ff61309e9d2642dea2
Dan Hope <dplhope@gmail.com>
2022-04-03 18:31:21 +0100

    getImages added

--- src/components/search.js
+++ src/components/search.js
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
 import "../styles/search.css";
+import getImages from "../requests/getImages";
 
 // eslint-disable-next-line react/function-component-definition
 const Search = () => {
-  // eslint-disable-next-line no-unused-vars
   const [value, setValue] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getImages(value);
+  };
   return (
-    <form className="search-form">
-      <input
-        className="search-input"
-        type="text"
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <button className="search-button" type="submit">
-        Search
-      </button>
-    </form>
+    <div className="Search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          className="search-input"
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
   );
 };
 
